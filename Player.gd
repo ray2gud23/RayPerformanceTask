050dc1deaee1889596c4d10b958fc71e3f21ddd3
Ray Earth Paul Omne <raomne@s.sfusd.edu>
2023-12-12 15:32:19 -0800

    Commit 4

--- Player.gd
+++ Player.gd
@@ -11,13 +11,17 @@ func _ready():
 
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.length > 0
-	if Input.is_action_pressed("move_left"):
-		velocity.length > 0
-	if Input.is_action_pressed("move_up"):
-		velocity.length > 0
-	if Input.is_action_pressed("move_down"):
-		velocity.length > 0
+
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x += -1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y += -1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
 	position += velocity * delta
-	
